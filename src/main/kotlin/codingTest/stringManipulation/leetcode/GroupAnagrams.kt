f168005ae6ddd3f3aca98052374b7ba2cd5e46ef
IMWoo94 <leemeawoo94@gmail.com>
2024-03-31 16:34:13 +0900

    coding : leetcode 49. Group Anagrams
    1. 객체 형태 이므로 제공되는 확장 함수를 통해 메소드 체이닝 방식으로 처리

---
+++ src/main/kotlin/codingTest/stringManipulation/leetcode/GroupAnagrams.kt
@@ -0,0 +1,23 @@
+package codingTest.stringManipulation.leetcode
+
+fun main() {
+    groupAnagrams(arrayOf("eat", "tea", "tan", "ate", "nat", "bat"))
+}
+
+fun groupAnagrams(strs: Array<String>): List<List<String>> {
+    // 애너그램 보관 map
+    val result: MutableMap<String, MutableList<String>> = mutableMapOf()
+
+    for (s in strs) {
+        // 문자열 정렬
+        val key = s.toCharArray().sorted().joinToString()
+
+        // map key 등록
+        result.getOrPut(key) { mutableListOf() }
+
+        // null 아님
+        result[key]!!.add(s)
+    }
+
+    return ArrayList<List<String>>(result.values)
+}
