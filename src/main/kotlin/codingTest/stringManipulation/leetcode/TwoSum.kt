68d2eaa87b20d95dc98be1e94f70795e47c857a9
IMWoo94 <leemeawoo94@gmail.com>
2024-04-01 18:41:54 +0900

    coding : leetcode 1. Two Sum
    1. map 활용

---
+++ src/main/kotlin/codingTest/stringManipulation/leetcode/TwoSum.kt
@@ -0,0 +1,22 @@
+package codingTest.stringManipulation.leetcode
+
+fun main() {
+    var nums = intArrayOf(2, 7, 11, 15)
+    var target = 9
+    twoSum(nums, target)
+}
+
+fun twoSum(nums: IntArray, target: Int): IntArray {
+    val map: MutableMap<Int, Int> = mutableMapOf()
+
+    // for 문 하나로 map, target 찾기
+    for ((i, num) in nums.withIndex()) {
+        var key = target - num
+        if (map.containsKey(key)) {
+            return intArrayOf(map.getOrDefault(key, 0), i)
+        }
+        map.put(num, i)
+    }
+
+    return intArrayOf(0, 0)
+}
