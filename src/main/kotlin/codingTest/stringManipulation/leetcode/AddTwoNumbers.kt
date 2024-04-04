3570e5bde6d9b862baad5eb3bdc6766ea740ad54
IMWoo94 <leemeawoo94@gmail.com>
2024-04-04 19:52:02 +0900

    coding : leetcode 2. Add Two Numbers
    1. 전가산기 기법

---
+++ src/main/kotlin/codingTest/stringManipulation/leetcode/AddTwoNumbers.kt
@@ -0,0 +1,35 @@
+package codingTest.stringManipulation.leetcode
+
+import codingTest.stringManipulation.common.ListNode
+
+fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
+
+    var root = ListNode(0)
+    var node = root
+
+    var node1 = l1
+    var node2 = l2
+
+    var sum: Int
+    var remainder: Int
+    var carry = 0
+    while (node1 != null || node2 != null || carry != 0) {
+        sum = 0
+        if (node1 != null) {
+            sum += node1.`val`
+            node1 = node1.next
+        }
+        if (node2 != null) {
+            sum += node2.`val`
+            node2 = node2.next
+        }
+
+        remainder = (sum + carry) % 10
+        carry = (sum + carry) / 10
+
+        node.next = ListNode(remainder)
+        node = node.next!!
+    }
+
+    return root.next
+}
