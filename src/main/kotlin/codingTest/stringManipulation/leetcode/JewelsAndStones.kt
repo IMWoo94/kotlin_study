a544314ffb70ffec94aa7ea772408808fe32bc9a
IMWoo94 <leemeawoo94@gmail.com>
2024-04-09 19:34:36 +0900

    coding : 771. Jewels and Stones

---
+++ src/main/kotlin/codingTest/stringManipulation/leetcode/JewelsAndStones.kt
@@ -0,0 +1,16 @@
+package codingTest.stringManipulation.leetcode
+
+fun numJewelsInStones(jewels: String, stones: String): Int {
+    var set: MutableSet<Char> = HashSet()
+    var count = 0
+
+    for (j in jewels.toCharArray()) {
+        set.add(j)
+    }
+
+    for (s in stones.toCharArray()) {
+        if (set.contains(s)) count++
+    }
+    return count
+
+}
