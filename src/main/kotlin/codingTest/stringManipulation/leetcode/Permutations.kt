570e40166d69535de51679b7eef716cfa9348ade
IMWoo94 <leemeawoo94@gmail.com>
2024-04-14 23:20:03 +0900

    coding : 46. Permutations

---
+++ src/main/kotlin/codingTest/stringManipulation/leetcode/Permutations.kt
@@ -0,0 +1,30 @@
+package codingTest.stringManipulation.leetcode
+
+import java.util.*
+import java.util.stream.Collectors
+
+fun permute(nums: IntArray): List<List<Int>> {
+    val results: MutableList<List<Int>> = mutableListOf()
+
+    fun dfs(prevElements: MutableList<Int>, element: MutableList<Int>) {
+        // 더 이상 요소가 없는 경우
+        if (element.isEmpty()) {
+            results.add(prevElements.stream().collect(Collectors.toList()))
+        }
+
+        for (e in element) {
+            var nextElement = ArrayList(element)
+            nextElement.remove(e)
+
+            prevElements.add(e)
+            dfs(prevElements, nextElement)
+            prevElements.remove(e)
+        }
+    }
+
+    val list = Arrays.stream(nums).boxed().collect(Collectors.toList())
+    dfs(mutableListOf(), list)
+
+    return results
+
+}
