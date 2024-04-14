f1701c5607207a8662dbef76d8d337a34d6c3f06
IMWoo94 <leemeawoo94@gmail.com>
2024-04-15 01:08:28 +0900

    coding : 46. Combinations

---
+++ src/main/kotlin/codingTest/stringManipulation/leetcode/Combinations.kt
@@ -0,0 +1,26 @@
+package codingTest.stringManipulation.leetcode
+
+import java.util.*
+import java.util.stream.Collectors
+
+fun combine(n: Int, k: Int): List<List<Int>> {
+
+    val results: MutableList<List<Int>> = mutableListOf()
+
+    fun dfs(element: LinkedList<Int>, start: Int, k: Int) {
+        if (k == 0) {
+            results.add(element.stream().collect(Collectors.toList()))
+            return
+        }
+
+        // 현재 요소 이후 탐색
+        for (i in start..n) {
+            element.add(i)
+            dfs(element, i + 1, k - 1)
+            element.removeLast()
+        }
+    }
+
+    dfs(LinkedList(), 1, k)
+    return results
+}
