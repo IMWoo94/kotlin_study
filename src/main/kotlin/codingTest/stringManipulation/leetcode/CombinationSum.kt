1c483fdb61294be9aafe2386396d7452e3847e44
IMWoo94 <leemeawoo94@gmail.com>
2024-04-15 16:43:01 +0900

    coding : 39. Combination Sum

---
+++ src/main/kotlin/codingTest/stringManipulation/leetcode/CombinationSum.kt
@@ -0,0 +1,26 @@
+package codingTest.stringManipulation.leetcode
+
+import java.util.*
+
+fun combinationSum(candidates: IntArray, target: Int): List<List<Int>> {
+    val results: MutableList<List<Int>> = mutableListOf()
+
+    fun dfs(target: Int, index: Int, path: Deque<Int>) {
+        if (target < 0) {
+            return
+        }
+        if (target == 0) {
+            results.add(ArrayList(path))
+            return
+        }
+
+        for (i in index until candidates.size) {
+            path.add(candidates[i])
+            dfs(target - candidates[i], i, path)
+            path.removeLast()
+        }
+    }
+
+    dfs(target, 0, ArrayDeque())
+    return results
+}
